e9aaba250c5c556f4731ae4640d87d385b9a4bf8
FreshLeaf8865 <cmd.bsolution@outlook.com>
2017-06-05 10:28:00 +0200

    JSON loading

--- app/components/const.js
+++ app/components/const.js
@@ -1,13 +1,12 @@
-const coords = []
+export const WIDTH = 500;
+export const VB_WIDTH = 1000;
 
-for (let i = 0; i < 100; i++) {
-    coords.push({
-        distance: Math.random(),
-        color: '#0000ff',
-        label: `Label ${i}`,
-        imageUrl: 'https://lh3.googleusercontent.com/dB3Dvgf3VIglusoGJAfpNUAANhTXW8K9mvIsiIPkhJUAbAKGKJcEMPTf0mkSexzLM5o=w300',
-        id: `${Math.random()}`
-    })
-}
+export const COLORS = [
+    ["#ededed", 6],
+    ["#f1dfc1", 5],
+    ["#facf84", 4],
+    ["#ffb33c", 3],
+    ["#ff9100", 2],
+    ["#ff6100", 1]
+];
 
-export default coords
